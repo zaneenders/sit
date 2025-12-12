aed4b92c79f9142fdf2aeb920e7105302d14e9af
Test User <test@example.com>
2025-12-11 20:35:06 -0700

    change access.

--- Sources/Sit/+String.swift
+++ Sources/Sit/+String.swift
@@ -2,11 +2,11 @@ import RegexBuilder
 
 /// Helpers to remove white space for dirty string comparisons.
 extension String {
-  public mutating func removeWhiteSpaces() {
+  package mutating func removeWhiteSpaces() {
     self.replace(Regex { OneOrMore(.whitespace) }) { _ in "" }
   }
 
-  public func removingWhiteSpaces() -> String {
+  package func removingWhiteSpaces() -> String {
     self.replacing(Regex { OneOrMore(.whitespace) }) { _ in "" }
   }
 }
